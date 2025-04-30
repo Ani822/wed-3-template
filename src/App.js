0b2d56a7a6f6bdabf2d66ff6638358ce0e5f2a76
Ani822 <anihovakimyan001@gmail.com>
2025-05-01 00:28:13 +0400

    contact

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from './components/footer/Footer';
 import Welcome from './components/welcome/Welcome';
 import Contacts from './components/contacts/Contacts';
+import CountDown from './components/count-down/CountDown';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Welcome />
       <Contacts />
+      <CountDown />
       <Footer />
     </div>
   );
